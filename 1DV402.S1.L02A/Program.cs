4e8f216341b06c48f462602ff6b6bb32c0cdc674
Mathias Duvdal <md222jv.student.lnu.se>
2015-02-02 13:06:33 +0100

    Klar och redovisad

---
+++ 1DV402.S1.L02A/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S1.L02A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 0; row < 25; row++)
+            {
+                if (row % 2 != 0)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int column = 0; column < 38; column++)
+                {
+                    switch (row % 3)
+                    {
+                        case 0:
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            break;
+                        case 1:
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            break;
+                        case 2:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            break;
+                    }
+                    Console.Write("* ");
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
